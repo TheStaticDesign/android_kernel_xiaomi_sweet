9f45724f791cc0e0e76138eb291f8626481fa4dc
CloudedQuartz <ravenklawasd@gmail.com>
2021-04-12 19:14:07 +0530

    kernel: Do not attempt to affine kthreads and IRQs to prime cluster
    - SD732G has only big and little clusters
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -33,7 +33,6 @@ struct irq_desc_list {
 static LIST_HEAD(perf_crit_irqs);
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 static int perf_cpu_index = -1;
-static int prime_cpu_index = -1;
 static bool perf_crit_suspended;
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
@@ -1198,10 +1197,7 @@ static void affine_one_perf_thread(struct irqaction *action)
 	if (!action->thread)
 		return;
 
-	if (action->flags & IRQF_PERF_AFFINE)
-		mask = cpu_perf_mask;
-	else
-		mask = cpu_prime_mask;
+	mask = cpu_perf_mask;
 
 	action->thread->flags |= PF_PERF_CRITICAL;
 	set_cpus_allowed_ptr(action->thread, mask);
@@ -1222,13 +1218,8 @@ static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
 	int *mask_index;
 	int cpu;
 
-	if (perf_flag & IRQF_PERF_AFFINE) {
-		mask = cpu_perf_mask;
-		mask_index = &perf_cpu_index;
-	} else {
-		mask = cpu_prime_mask;
-		mask_index = &prime_cpu_index;
-	}
+	mask = cpu_perf_mask;
+	mask_index = &perf_cpu_index;
 
 	if (!cpumask_intersects(mask, cpu_online_mask)) {
 		WARN(1, "requested perf CPU is offline for %s\n", desc->name);
@@ -1303,7 +1294,6 @@ void reaffine_perf_irqs(bool from_hotplug)
 	if (!from_hotplug || !perf_crit_suspended) {
 		perf_crit_suspended = false;
 		perf_cpu_index = -1;
-		prime_cpu_index = -1;
 		list_for_each_entry(data, &perf_crit_irqs, list) {
 			struct irq_desc *desc = data->desc;
 
@@ -1573,7 +1563,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			irqd_set(&desc->irq_data, IRQD_NO_BALANCING);
 		}
 
-		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE)) {
+		if (new->flags & (IRQF_PERF_AFFINE)) {
 			affine_one_perf_thread(new);
 			irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
 			*old_ptr = new;
