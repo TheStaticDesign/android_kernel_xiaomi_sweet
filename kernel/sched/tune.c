e42eebd08b3b2483023e72a3e25a77545c80e522
CloudedQuartz <ravenklawasd@gmail.com>
2021-05-28 09:59:45 +0530

    sched: tune: stune_assist: guard no_override write under sched_walt
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -702,6 +702,7 @@ static int prefer_high_cap_write(struct cgroup_subsys_state *css,
 }
 
 #ifdef CONFIG_STUNE_ASSIST
+#ifdef CONFIG_SCHED_WALT
 static int sched_boost_override_write_wrapper(struct cgroup_subsys_state *css,
 					      struct cftype *cft, u64 override)
 {
@@ -711,7 +712,6 @@ static int sched_boost_override_write_wrapper(struct cgroup_subsys_state *css,
 	return sched_boost_override_write(css, cft, override);
 }
 
-#ifdef CONFIG_SCHED_WALT
 static int sched_colocate_write_wrapper(struct cgroup_subsys_state *css,
 					struct cftype *cft, u64 colocate)
 {
@@ -818,12 +818,12 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
 			boost_write(css, NULL, tgt.boost);
 			prefer_idle_write(css, NULL, tgt.prefer_idle);
-			sched_boost_override_write(css, NULL, tgt.no_override);
 #ifndef CONFIG_SCHED_WALT
-			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d no_override=%d\n",
-				tgt.name, tgt.boost, tgt.prefer_idle, tgt.no_override);
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle);
 #else
 			sched_colocate_write(css, NULL, tgt.colocate);
+			sched_boost_override_write(css, NULL, tgt.no_override);
 			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d colocate=%d no_override=%d\n",
 				tgt.name, tgt.boost, tgt.prefer_idle, tgt.colocate, tgt.no_override);
 #endif
