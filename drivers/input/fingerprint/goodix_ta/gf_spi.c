94b1ae0f2a6349b38af31bca7777eb38b7af3151
CloudedQuartz <ravenklawasd@gmail.com>
2021-11-09 18:43:16 +0530

    drivers: fingerprint: goodix_ta: relax wakelock
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- drivers/input/fingerprint/goodix_ta/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -63,8 +63,8 @@
 #define VER_MINOR   2
 #define PATCH_LEVEL 1
 
-#define WAKELOCK_HOLD_TIME 2000 /* in ms */
-#define FP_UNLOCK_REJECTION_TIMEOUT (WAKELOCK_HOLD_TIME - 500)
+#define WAKELOCK_HOLD_TIME 500 /* in ms */
+#define FP_UNLOCK_REJECTION_TIMEOUT (WAKELOCK_HOLD_TIME - 100)
 
 #define GF_SPIDEV_NAME			"goodix,fingerprint"
 /*device name after register in charater*/
